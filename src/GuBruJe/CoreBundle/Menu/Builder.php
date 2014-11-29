86522dfccc88e88bda32f5cbcdfcbc7ec6c39cf3
Jéremy <gubruje@gmail.com>
2014-11-29 19:39:49 +0100

    Set the form authentication in the navbar and replace some fosusers templates

--- src/GuBruJe/CoreBundle/Menu/Builder.php
+++ src/GuBruJe/CoreBundle/Menu/Builder.php
@@ -21,4 +21,17 @@ class Builder extends ContainerAware
 
         return $menu;
     }
+
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+        $username = $this->container->get('security.context')->getToken()->getUser()->getUsername();
+        $menu = $factory->createItem('root');
+
+        $user = $menu->addChild('user', array('uri' => '#','label' => '.icon-user '.$username));
+        $user->addChild('Mon compte', array('route' => 'fos_user_profile_show'));
+        $user->addChild('logout', array('route' => 'fos_user_security_logout', 'label' => 'layout.logout'))->setExtra('translation_domain', 'FOSUserBundle');;
+        // ... add more children
+
+        return $menu;
+    }
 }
