a6af96adedac12e46f1f6eb3a3c9abd4a2269f31
Jéremy <gubruje@gmail.com>
2015-02-14 10:01:13 +0100

    Creation de l'annonce admin

---
+++ src/GuBruJe/MusicalBundle/Admin/AnnonceAdmin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeremy
+ * Date: 31/01/15
+ * Time: 19:49
+ */
+
+namespace GuBruJe\MusicalBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class AnnonceAdmin extends Admin {
+// Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+
+            ->add('statut' , 'entity', array(
+                'class' => 'GuBruJeMusicalBundle:Statut',
+                'property' => 'nom',
+                'expanded' => true,
+            ))
+            ->add('titre', 'text', array('label' => 'Titre'))
+            ->add('contenu') //if no type is specified, SonataAdminBundle tries to guess it
+            ->add('auteur', 'entity', array('class' => 'Application\Sonata\UserBundle\Entity\User'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('statut')
+            ->add('auteur')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('titre')
+            ->add('auteur')
+            ->add('statut')
+
+        ;
+    }
+} 
